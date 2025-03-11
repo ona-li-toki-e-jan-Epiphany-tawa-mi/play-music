7178161c1e4987f38933588c401b22c0884c1f16
jan Epiphany <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2025-03-11 09:06:33 -0400

    Added license

--- play-music.c
+++ play-music.c
@@ -1,3 +1,21 @@
+/*
+ * This file is part of play-music.
+ *
+ * Copyright (c) 2025 ona-li-toki-e-jan-Epiphany-tawa-mi
+ *
+ * play-music is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * play-music is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * play-music. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #define _POSIX_C_SOURCE 199309L
 #include <assert.h>
 #include <stdbool.h>
